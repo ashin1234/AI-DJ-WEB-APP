c2751e724a44453be7f7ee9c3f41fc5bd6181bfc
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-04-08 20:41:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-song = "";
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
-function preload(){
-  song = loadSound("music.mp3");
+function preload() {
+    song = loadSound("music.mp3", "music2.mp3");
 }
-
-function setup(){
+function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('poseNet Is Initialized');
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX + " leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX + " rightWristY = " + rightWristY);
+
+    }
 }
 
 function draw() {
@@ -17,4 +38,6 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
